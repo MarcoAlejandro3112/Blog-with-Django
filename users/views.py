3c62ff4309fb3dcfe13ea5ee559958b9ec13f58e
Marco Altamirano <maaltamiranohiga@gmail.com>
2021-01-13 23:21:57 -0500

    updating profile

--- users/views.py
+++ users/views.py
@@ -17,8 +17,19 @@ def register(request):
     return render(request,'register.html',{'form':form,'bootstrap':'yes'})
 @login_required
 def profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateform()
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = ProfileUpdateform(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateform(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
