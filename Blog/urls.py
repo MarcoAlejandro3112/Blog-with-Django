86a91647bf621436da15b4671d2fd991aedf9611
Marco Altamirano <maaltamiranohiga@gmail.com>
2021-01-14 21:34:10 -0500

    creating,updating and deleting post

--- Blog/urls.py
+++ Blog/urls.py
@@ -22,7 +22,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name="blogApp_index"),
+    path('',views.PostListView.as_view(),name="blogApp_index"),
+    path('post/<int:pk>/',views.PostDetailView.as_view(),name='post-detail'),
+    path('post/new/',views.PostCreateView.as_view(),name='post-create'),
+    path('post/<int:pk>/update/',views.PostUpdateView.as_view(),name='post-update'),
+    path('post/<int:pk>/delete/',views.PostDeleteView.as_view(),name="post-delete"),
     path('acerca/',views.about,name="blogApp_about"),
     path('register/',viewsU.register,name="users_register"),
     path('profile/',viewsU.profile,name='profile'),
--- blogApp/models.py
+++ blogApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 class Categoria(models.Model):
     name = models.CharField(max_length=100)
     def __str__(self):
@@ -9,10 +9,13 @@ class Categoria(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    summary = models.CharField(max_length=30)
+    summary = models.CharField(max_length=100)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail',kwargs={'pk':self.pk})
